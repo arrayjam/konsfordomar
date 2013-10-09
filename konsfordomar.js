c86b9893cd458889b15e3c943f133ccc14d3e286
Yuri Feldman <arrayjam@tbqh.net>
2013-10-10 05:35:59 +1100

    better????

--- konsfordomar.js
+++ konsfordomar.js
@@ -1,5 +1,5 @@
 var levels = 3,
-  subordinates = 3,
+  subordinates = 2,
   width = 940,
   height = 500,
   id = 0;
@@ -8,14 +8,13 @@ var randomGender = function () {
   return (Math.random() > 0.5) ? "f" : "m";
 };
 
-var root = newNode(id);
+var root = newNode();
 
 var createLevel = function (node, level) {
   node.children = [];
   if (level + 1 >= levels) return;
   d3.range(subordinates).forEach(function() {
-    id += 1;
-    var child = newNode(id);
+    var child = newNode();
     node.children.push(child);
     createLevel(child, level + 1);
   });
@@ -29,7 +28,7 @@ var tree = d3.layout.tree()
 var diagonal = d3.svg.diagonal()
   // Swapsies
   .source(function(d) { return d.target; })
-  .target(function(d) { return d.source; })
+  .target(function(d) { return d.source; });
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -39,12 +38,16 @@ var svg = d3.select("body").append("svg")
 
 
 
-var nodes, links;
+var nodes, links, last;
 function update () {
   nodes = tree.nodes(root), links = tree.links(nodes);
-  console.log("nodes", nodes.length);
-  console.log("links", links.length);
-
+  var ids = nodes.map(function(d) { return d.id; });
+  var changed = [];
+  if (last) {
+    ids.forEach(function(d, i) {
+      if (ids[i] !== last[i]) changed.push(d);
+    });
+  }
   var link = svg.selectAll(".link")
       .data(links)
     .enter().append("path")
@@ -54,14 +57,9 @@ function update () {
       .attr("d", diagonal);
 
   var node = svg.selectAll(".node")
-      .data(nodes, function(d) { return d.id; });
-
-  node.enter().append("circle")
-      .attr("r", 0)
-      .transition()
-      .attr("r", 6.5);
-
-  node.attr("id", function(d) { return "node-" + d.id; })
+      .data(nodes, function(d) { return d.id; })
+    .enter().append("circle")
+      .attr("r", 6.5)
       .attr("class", "node")
       .attr("cx", function(d) { return d.x; })
       .on("click", function(d) { console.log(d); })
@@ -69,43 +67,72 @@ function update () {
       // yes yes
       .style("fill", function(d) { return (d.gender === "m") ? "steelblue" : "white"; });
 
+  svg.selectAll(".node")
+      .data(nodes, function(d) { return d.id; })
+      .attr("id", function(d) { return "node-" + d.id; });
+  var pairs = d3.pairs(changed.reverse());
+
+  console.log(changed);
+  if (changed.length) remove(findNodeById(root, changed.reverse()[0]));
+
+  pairs.forEach(function(d) {
+    var up = findNodeById(root, d[0]);
+    var down = findNodeById(root, d[1]);
+    move(up, down);
+  });
+
+  last = ids;
+
 }
 
-function removal (nodes) {
+function tick () {
   var id = selectRandomId(nodes);
   var node = findNodeById(root, id);
-  d3.select("#node-" + node.id).transition().attr("r", 0).each("end", update).remove();
+  promoteTo(node);
+  update();
+}
+
+function promoteTo(node) {
   if (node.children.length) {
-    var toPromote = randomElement(node.children);
-    //console.log("replacing", node, "with", toPromote);
-    promote(toPromote, node);
+    promote(randomElement(node.children), node);
   } else {
     node.gender = randomGender();
     node.id = id++;
   }
 }
 
-function promote (sub, current) {
-  //console.log(sub, current);
-  var link = tree.links([current]).filter(function(d) { return d.target.id === sub.id; })[0];
+function remove (node) {
+  d3.select("#node-" + node.id).transition().attr("r", 0).remove();
+}
+
+function move (down, up) {
+  var link = tree.links([up]).filter(function(d) { return d.target.id === down.id; })[0];
+  console.log(link);
   var path = svg.append("path").attr("class", "hidden").attr("d", diagonal(link));
-  d3.select("#node-" + sub.id).transition().duration(1000)
+  console.log(d3.select("#node-" + down.id));
+  d3.select("#node-" + down.id).transition().duration(1000)
     .attrTween("cx", translateXAlong(path.node()))
     .attrTween("cy", translateYAlong(path.node()))
     .each("end", function() { path.remove(); });
-  current.gender = sub.gender;
-  console.log(sub.id, "->", current.id);
-  current.id = sub.id;
-  if (sub.children.length) {
-    promote(randomElement(sub.children), sub);
+}
+
+function promote (down, up) {
+  //move(down, up);
+  (function(down, up) {
+    console.log(id);
+    up.gender = down.gender;
+    up.id = id++;
+  })(down, up);
+  if (down.children.length) {
+    promote(randomElement(down.children), down);
   } else {
-    sub.id = id += 1;
-    sub.gender = randomGender();
+    down.gender = randomGender();
+    down.id = id++;
   }
 }
 
 function selectRandomId (nodes) {
-  //nodes = nodes.filter(function(d) { return d.depth < 1; });
+  //nodes = nodes.filter(function(d) { return d.depth === 0; });
   return randomElement(nodes).id;
 }
 
@@ -118,8 +145,8 @@ function findNodeById (node, id) {
   }
 }
 
-function newNode(id) {
-  return {id: id, gender: randomGender()};
+function newNode() {
+  return {id: id++, gender: randomGender()};
 }
 
 function randomElement (array) {
